9ac6e8413b076237ff73b5236e323ca25a93e034
skysgit <393740104@qq.com>
2020-02-23 22:49:43 +0800

    config-server提交

--- src/main/java/com/cn/app/server/ConfigServerApp.java
+++ src/main/java/com/cn/app/server/ConfigServerApp.java
@@ -1,8 +1,7 @@
 package com.cn.app.server;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
@@ -11,8 +10,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  * 为了写点体会和这里没必要用到包和类，所以使用这种方式
  */
 @EnableConfigServer
-@SpringBootConfiguration
-@EnableAutoConfiguration
+@SpringBootApplication
 public class ConfigServerApp {
 	  public static void main(String[] args) {
 	        SpringApplication.run(ConfigServerApp.class, args);
